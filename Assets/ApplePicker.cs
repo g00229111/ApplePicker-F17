aa51176cf4625f7f05090b1aac1744a801912484
g00229111 <raisingsun010@hotmail.com>
2017-09-22 12:43:46 -0400

    Apple Picker
    Apple Picker

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplePicker : MonoBehaviour {
+public class ApplePicker : MonoBehaviour
+{
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        basketList = new List<GameObject>();
+        for(int i=0; i<numBaskets; i++)
+        {
+            GameObject tBasketGo = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+    }
 }
---
+++ Assets/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    void Awake()
+    {
+        if(PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+    }
+
+}
